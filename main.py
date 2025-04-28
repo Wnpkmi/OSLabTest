67202f65e3024d7d9b2e4ee54d323888c7547591
radinkaj <radin.kajbaf@outlook.com>
2025-04-28 16:06:29 +0330

    nima mehrsa

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ student1 = {'name': 'soheil', 'lastname': 'latifi', 'sid': 14010122680417}
 student2 = {'name': 'hooman', 'lastname': 'edraki', 'sid': 14010122268108}
 student3 = {'name': 'maria','lastname':' delkash', 'sid':1401012268099}
 student4 = {'name':'sophia', 'lastname':'delkash',  'sid': 1401012268081}
+student5 = {'name':'mehrsa', 'lastname':'joolani',  'sid': 1401012268057}
+
+student6 = {'name':'Nima', 'lastname':'Haddad',  'sid': 1401012268054}
 
 lis = []
 # append your varible
@@ -9,7 +12,8 @@ lis.append(student1)
 lis.append(student2)
 lis.append(student3)
 lis.append(student4)
-
+lis.append(student5)
+lis.append(student6)
 
 
 people_sorted = sorted(lis, key=lambda x: x['name'])
